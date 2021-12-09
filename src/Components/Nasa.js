437b0a7b0a60bdcbdcd75cf9d9ede8f21433eb17
kevin-liu <liu.kevin1997@gmail.com>
2021-12-09 18:45:12 -0500

    finished styling

--- src/Components/Nasa.js
+++ src/Components/Nasa.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyleContainer = styled.div`
+   p {
+     background-color: ${props => props.theme.primaryColor};
+     color: ${props => props.theme.white};
+   }
+   h1 {
+     background-color: ${props => props.theme.black};
+   }
+   img:hover{
+     border-radius: 50%;
+   }
+`
 
 const Nasa = (props) => {
   return (
-    <div>
+    <StyleContainer>
       <h1>NASA FUN</h1>
       <img src={props.nasaData.url} alt="Nasa APOD" />
       <p>{props.nasaData.date}</p>
       <p>{props.nasaData.explanation}</p>
-    </div>
+    </StyleContainer>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import styling from './styling/index';
+import { ThemeProvider } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ThemeProvider theme={styling}><App /></ThemeProvider>, document.getElementById("root"));
